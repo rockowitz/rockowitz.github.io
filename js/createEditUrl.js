ccd750540c252f977fa89129922dfbfc27cf93e9
Amir Omidi <amir@aaomidi.com>
2021-12-07 03:35:42 -0500

    Fix edit on github button

---
+++ js/createEditUrl.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(_){
+  let element = document.getElementById("edit-gh");
+
+  element.href = `https://github.com/rockowitz/rockowitz.github.io/blob/master${location.pathname}index.html`
+});
